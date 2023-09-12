77410c3828ad08e2601d1ac4692bc8be6b1f9477
Vitor Marques <vitorsantoscomu@gmail.com>
2023-09-12 16:54:45 -0300

    Usando massa de dados

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,30 +1,45 @@
 /// <reference types = "cypress"/>
+const perfil = require('../fixtures/perfil.json')
 
-context ('Funcionalidade Login', () => {
+context('Funcionalidade Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
         cy.screenshot()
     });
-    
-    
+
+
     it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá,')
     })
+
+    it.only('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+    })
+
+    it.only('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.page-title').should('contain', 'Minha conta')
+        })
+    });
 
     it('Deve exibir uma mensagem de erro ao inserir um usuário inválido', () => {
         cy.get('#username').type('usuarioinvalido@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-error > li').should('contain', 'Endereço de')
     })
 
@@ -32,8 +47,7 @@ context ('Funcionalidade Login', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('.woocommerce-form > :nth-child(2) > label').type('senhainvalida')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha')
     })
 
-} )
+})
