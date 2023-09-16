3d1fab33da7d7103b332fbc9c34dee6b743bf1f2
Vitor Marques <vitorsantoscomu@gmail.com>
2023-09-16 14:17:49 -0300

    Massa de dados em listas

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -2,6 +2,8 @@
 
 import EnderecoPage from '../support/page-objects/endereco.page'
 
+const dadosEndereco = require('../fixtures/endereco.json')
+
 describe('Funcionalidade endereços - Faturamente e Entrega', () => {
 
     beforeEach(() => {
@@ -9,11 +11,28 @@ describe('Funcionalidade endereços - Faturamente e Entrega', () => {
         cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
-        
+
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFaturamento('Augusto', 'Lima', 'Carrefour', 'Brasil', 'Avenida Atlântica', '501 Qd 15', 'Goiânia', 'Goiás', '74795-350', '5555-8888', 'emaildeteste2@ebac.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+        )
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
