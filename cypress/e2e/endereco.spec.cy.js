9034041d8f124c3f77a9d123fc836df559c352b3
Vitor Marques <vitorsantoscomu@gmail.com>
2023-09-16 13:15:48 -0300

    Page Objects

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,5 +1,7 @@
 /// <reference types = "cypress"/>
 
+import EnderecoPage from '../support/page-objects/endereco.page'
+
 describe('Funcionalidade endereços - Faturamente e Entrega', () => {
 
     beforeEach(() => {
@@ -10,7 +12,9 @@ describe('Funcionalidade endereços - Faturamente e Entrega', () => {
         
     });
 
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Augusto', 'Lima', 'Carrefour', 'Brasil', 'Avenida Atlântica', '501 Qd 15', 'Goiânia', 'Goiás', '74795-350', '5555-8888', 'emaildeteste2@ebac.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
+
 });
